3ada91c49aba44062d1b4c2866136def8ec0c714
kөrme <61244717+qalpan@users.noreply.github.com>
2020-02-24 18:24:48 +0600

    Add files via upload

---
+++ oner-bilim-sport.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://qalpan.github.io/korme/suretter/favicon.gif" type="image/gif">
+	<title>Қaлпан</title>
+	<script src="https://qalpan.github.io/korme/js/jquery.min.js"></script>
+	<script src="https://qalpan.github.io/korme/js/pgwslider.js"></script>
+	<link rel="stylesheet" href="https://qalpan.github.io/korme/css/pgwslider.css">
+</head>
+
+<SCRIPT type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+return false}
+</SCRIPT>
+
+<body>
+  <div class="main">
+   <ul class="pgwSlider">
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/алқалар тұқымдасы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/бұршақ тұқымдастар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/бірус.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/жабық тұқымды өсімдіктер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/жануарлардың қозғалуы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/жұйке жүйесі-жұлын.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/жүйке жұйесі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/жыл бастау.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/жыл мезгілдері.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/жыл мезгілдері1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/көркем гимнастика.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/күрделі гүлділер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/қайың.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/қосылыс формулалары.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/қыналар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/ми.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/мұзда сырғанаушы қыз.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/нейрондар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/омыртқалы және омыртқасыздар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/өмір жолы-бағыт.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/пайнберрий.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/пиаз тұқымдастар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/поема.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/раушан гүлділер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/сыйлықтар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/торсық.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/хромасом.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/шешім.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/oner bilim sport/шымтезек.jpg"/></li>
+	</ul>
+
+<li class="тақырыптар">
+<a class="адамдар" href="https://qalpan.github.io/korme/index.html">адамдар ....</a>
+<a class="бөлме-ғимарат-жобалар" href="https://qalpan.github.io/korme/bolme-gimarat-jobalar.html">бөлме ғимарат жобалар</a>
+<a class="ғарыш-қиал-тылсым-әлем" href="https://qalpan.github.io/korme/garyh-kial-tylsym-alem.html">ғарыш қиал тылсым әлем</a>
+<a class="гүлдер" href="https://qalpan.github.io/korme/gylder.html">гүлдер</a>
+<a class="жеміс-жидек-өсімдік-ағаштар" href="https://qalpan.github.io/korme/jemis-jidek-osimdik-agahtar.html">жеміс жидек өсімдік ағаштар</a>
+<a class="қалыпталған-кескіндер" href="https://qalpan.github.io/korme/kalyptalgan-keskinder.html">қалыпталған кескіндер</a>
+<a class="қолданыс-заттары-құрылғы-құралдар" href="https://qalpan.github.io/korme/koldanys-zattary-kyrylgy-kyraldar.html 
+">қолданыс заттары құрылғы құралдар</a>
+<a class="құс-жануар-жәндік-балықтар" href="https://qalpan.github.io/korme/kys-januar-jandik-balyktar.html">құс жануар жәндік балықтар</a>
+<a class="өнер-білім-спорт" href="https://qalpan.github.io/korme/өнер-білім-спорт"><p class="takyryp-janu">өнер білім спорт</p></a>
+<a class="сән-үлгілері" href="https://qalpan.github.io/korme/сән-үлгілері">сән үлгілері</a>
+<a class="табиғат" href="https://qalpan.github.io/korme/табиғат">табиғат</a></li>
+
+<ul id="атаубелгі-karlyga">
+<a class="белгі-karlyga">
+<span class="karlyga">Қalpan</span>kөrмеsі</a>
+</ul>
+</div>
+
+<script>
+	$(document).ready(function() {
+		$('.pgwSlider').pgwSlider({
+			autoSlide: true,
+			displayControls: true
+		});
+	});
+</script>
+</body>
+</html>
