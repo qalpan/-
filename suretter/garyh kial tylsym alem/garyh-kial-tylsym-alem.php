30c7c2fd958c2901218ac8a8071e4d7540f14e36
kөrme <61244717+qalpan@users.noreply.github.com>
2020-02-24 16:51:17 +0600

    Add files via upload

---
+++ suretter/garyh kial tylsym alem/garyh-kial-tylsym-alem.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="/suretter/favicon.gif" type="image/gif">
+	<title>Қaлпан</title>
+	<script src="https://qalpan.github.io/korme/js/jquery.min.js"></script>
+	<script src="https://qalpan.github.io/korme/js/pgwslider.js"></script>
+	<link rel="stylesheet" href="https://qalpan.github.io/korme/css/pgwslider.css">
+</head>
+
+<SCRIPT type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+return false}
+</SCRIPT>
+
+<body>
+<div class="main">
+	<ul class="pgwSlider">
+
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/ғаламшар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/ғарыш нысандары.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/ғарыш.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/ғарыштағы құрылғы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/түстер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/түстерӘ.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/түстерҒ.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/garyh kial tylsym alem/түстерді салыстыру.jpg"/></li>
+
+	</ul>
+
+<li class="тақырыптар">
+<a class="адамдар" href="https://qalpan.github.io/korme/адамдар">адамдар ....</a>
+<a class="бөлме-ғимарат-жобалар" href="https://qalpan.github.io/korme/бөлме-ғимарат-жобалар">бөлме ғимарат жобалар</a>
+<a class="ғарыш-қиал-тылсым-әлем" href="https://qalpan.github.io/korme/ғарыш-қиал-тылсым-әлем"><p class="takyryp-janu">ғарыш қиал тылсым әлем</p></a>
+<a class="гүлдер" href="https://qalpan.github.io/korme/гүлдер">гүлдер</a>
+<a class="жеміс-жидек-өсімдік-ағаштар" href="https://qalpan.github.io/korme/жеміс-жидек-өсімдік-ағаштар">жеміс жидек өсімдік ағаштар</a>
+<a class="қалыпталған-кескіндер" href="https://qalpan.github.io/korme/қалыпталған-кескіндер">қалыпталған кескіндер</a>
+<a class="қолданыс-заттары-құрылғы-құралдар" href="https://qalpan.github.io/korme/қолданыс-заттары-құрылғы-құралдар">қолданыс заттары құрылғы құралдар</a>
+<a class="құс-жануар-жәндік-балықтар" href="https://qalpan.github.io/korme/құс-жануар-жәндік-балықтар">құс жануар жәндік балықтар</a>
+<a class="өнер-білім-спорт" href="https://qalpan.github.io/korme/өнер-білім-спорт">өнер білім спорт</a>
+<a class="сән-үлгілері" href="https://qalpan.github.io/korme/сән-үлгілері">сән үлгілері</a>
+<a class="табиғат" href="https://qalpan.github.io/korme/табиғат">табиғат</a></li>
+
+<ul id="атаубелгі-karlyga">
+<a class="белгі-karlyga">
+<span class="karlyga">Қalpan</span>kөrмеsі</a>
+</ul>
+
+</div>
+
+<script>
+	$(document).ready(function() {
+		$('.pgwSlider').pgwSlider({
+			autoSlide: true,
+			displayControls: true
+		});
+	});
+</script>
+</body>
+</html>
