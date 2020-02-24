a6b8a9874448354c80d0cd55110d6312e36c938f
kөrme <61244717+qalpan@users.noreply.github.com>
2020-02-24 18:37:30 +0600

    Add files via upload

---
+++ tabigat.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://qalpan.github.io/korme/suretter/favicon.gif" type="image/gif">
+	<title>Қaлпан</title>
+	<script src="https://qalpan.github.io/korme/js/jquery.min.js"></script>
+	<script src="https://qalpan.github.io/korme/js/pgwslider.js"></script>
+	<link rel="stylesheet" href="https://qalpan.github.io/korme/css/pgwslider.css">
+</head>
+
+<SCRIPT type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+return false}
+</SCRIPT>
+
+<body>
+<div class="main">
+	<ul class="pgwSlider">
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/асқар тау.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/ауыл өзені.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/ауыл өзені1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/ауыл өзені2.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/ауыл өзені3.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/ауылда.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/ауылым.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/әр түсті жаз.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/бұқтырма.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/бұл дала.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/дара емен.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/дауыл.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/елдімекен жолы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жағалау.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жауын.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жауыннан соң.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жәйләу.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жәйләу1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жәйләу2.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жәйләу3.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/жәйләу4.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/кемпірқосақ.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/кемпірқосақ1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/көктемгі бақ.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/күз.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/күз1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/күз2.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/күн.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/кіші қала.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/қайық тұрағы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/құстар мекені.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/қыс.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/нөсер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/орман дауылы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/орман көлі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/рахман жағажәйі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/рахман көлі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/сайа бақ.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/сайа жәй.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/су жолы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тал айасы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тасқын.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тау бөктері.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тау етегі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тау өзені.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тау өзені1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/таудағы үй.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/теңіз.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тоғай.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/толқын.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/толқын1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тропика аралы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/тропика үйі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/tabigat/түнгі аспан.jpg"/></li>
+	</ul>
+
+<li class="тақырыптар">
+<a class="адамдар" href="https://qalpan.github.io/korme/index.html">адамдар ....</a>
+<a class="бөлме-ғимарат-жобалар" href="https://qalpan.github.io/korme/bolme-gimarat-jobalar.html">бөлме ғимарат жобалар</a>
+<a class="ғарыш-қиал-тылсым-әлем" href="https://qalpan.github.io/korme/garyh-kial-tylsym-alem.html">ғарыш қиал тылсым әлем</a>
+<a class="гүлдер" href="https://qalpan.github.io/korme/gylder.html">гүлдер</a>
+<a class="жеміс-жидек-өсімдік-ағаштар" href="https://qalpan.github.io/korme/jemis-jidek-osimdik-agahtar.html">жеміс жидек өсімдік ағаштар</a>
+<a class="қалыпталған-кескіндер" href="https://qalpan.github.io/korme/kalyptalgan-keskinder.html">қалыпталған кескіндер</a>
+<a class="қолданыс-заттары-құрылғы-құралдар" href="https://qalpan.github.io/korme/koldanys-zattary-kyrylgy-kyraldar.html 
+">қолданыс заттары құрылғы құралдар</a>
+<a class="құс-жануар-жәндік-балықтар" href="https://qalpan.github.io/korme/kys-januar-jandik-balyktar.html">құс жануар жәндік балықтар</a>
+<a class="өнер-білім-спорт" href="https://qalpan.github.io/korme/oner-bilim-sport.html">өнер білім спорт</a>
+<a class="сән-үлгілері" href="https://qalpan.github.io/korme/san-ylgileri.html">сән үлгілері</a>
+<a class="табиғат" href="https://qalpan.github.io/korme/табиғат"><p class="takyryp-janu">табиғат</p></a></li>
+
+<ul id="атаубелгі-karlyga">
+<a class="белгі-karlyga">
+<span class="karlyga">Қalpan</span>kөrмеsі</a>
+</ul>
+</div>
+
+<script>
+	$(document).ready(function() {
+		$('.pgwSlider').pgwSlider({
+			autoSlide: true,
+			displayControls: true
+		});
+	});
+</script>
+
+</body>
+</html>
