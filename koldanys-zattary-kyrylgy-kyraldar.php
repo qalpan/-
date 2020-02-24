29c5b10207be25c21ddc8a20c61dd125909e6789
kөrme <61244717+qalpan@users.noreply.github.com>
2020-02-24 18:01:12 +0600

    Add files via upload

---
+++ koldanys-zattary-kyrylgy-kyraldar.php
@@ -0,0 +1,80 @@
+<?php define('karlygakorme', true);
+defined('karlygakorme') or die('көруге рұқсат жоқ!');
+ ?>
+
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://qalpan.github.io/korme/suretter/favicon.gif" type="image/gif">
+	<title>Қaлпан</title>
+	<script src="https://qalpan.github.io/korme/js/jquery.min.js"></script>
+	<script src="https://qalpan.github.io/korme/js/pgwslider.js"></script>
+	<link rel="stylesheet" href="https://qalpan.github.io/korme/css/pgwslider.css">
+</head>
+
+<SCRIPT type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+return false}
+</SCRIPT>
+
+<body>
+<div class="main">
+	<ul class="pgwSlider">
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/дастархан.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/доп.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/доп1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/ерке мысық.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/қонақ есік.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/құмыра.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/құмыра1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/мейрамда.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/оқушы құралдары.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/сары қызыл шәйнек.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/сынған ыдыс.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/түсті шәйнек.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/шарлар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/шәйнек ыдыс.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/koldanys zattary/шәйнек.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kyrylgy kyraldar/жасыл зымыран.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kyrylgy kyraldar/қара зымыран.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kyrylgy kyraldar/қойыу көк зымыран.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kyrylgy kyraldar/сары зымыран.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kyrylgy kyraldar/сары зымыран1.jpg"/></li>
+	</ul>
+
+<li class="тақырыптар">
+<a class="адамдар" href="https://qalpan.github.io/korme/index.html">адамдар ....</a>
+<a class="бөлме-ғимарат-жобалар" href="https://qalpan.github.io/korme/бөлме-ғимарат-жобалар">бөлме ғимарат жобалар</a>
+<a class="ғарыш-қиал-тылсым-әлем" href="https://qalpan.github.io/korme/ғарыш-қиал-тылсым-әлем">ғарыш қиал тылсым әлем</a>
+<a class="гүлдер" href="https://qalpan.github.io/korme/гүлдер">гүлдер</a>
+<a class="жеміс-жидек-өсімдік-ағаштар" href="https://qalpan.github.io/korme/жеміс-жидек-өсімдік-ағаштар">жеміс жидек өсімдік ағаштар</a>
+<a class="қалыпталған-кескіндер" href="https://qalpan.github.io/korme/қалыпталған-кескіндер">қалыпталған кескіндер</a>
+<a class="қолданыс-заттары-құрылғы-құралдар" href="https://qalpan.github.io/korme/қолданыс-заттары-құрылғы-құралдар"><p class="takyryp-janu">қолданыс заттары құрылғы құралдар</p></a>
+<a class="құс-жануар-жәндік-балықтар" href="https://qalpan.github.io/korme/құс-жануар-жәндік-балықтар">құс жануар жәндік балықтар</a>
+<a class="өнер-білім-спорт" href="https://qalpan.github.io/korme/өнер-білім-спорт">өнер білім спорт</a>
+<a class="сән-үлгілері" href="https://qalpan.github.io/korme/сән-үлгілері">сән үлгілері</a>
+<a class="табиғат" href="https://qalpan.github.io/korme/табиғат">табиғат</a></li>
+
+<ul id="атаубелгі-karlyga">
+<a class="белгі-karlyga">
+<span class="karlyga">Қalpan</span>kөrмеsі</a>
+</ul>
+
+</div>
+
+<script>
+	$(document).ready(function() {
+		$('.pgwSlider').pgwSlider({
+			autoSlide: true,
+			displayControls: true
+		});
+	});
+</script>
+</body>
+</html>
