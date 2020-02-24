78e43a63c24020e6e2b85a2a66757931facaef3b
kөrme <61244717+qalpan@users.noreply.github.com>
2020-02-24 17:39:16 +0600

    Add files via upload

---
+++ jemis-jidek-osimdik-agahtar.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://qalpan.github.io/korme/suretter/favicon.gif" type="image/gif">
+	<title>Қaлпан</title>
+	<script src="https://qalpan.github.io/korme/js/jquery.min.js"></script>
+	<script src="https://qalpan.github.io/korme/js/pgwslider.js"></script>
+	<link rel="stylesheet" href="https://qalpan.github.io/korme/css/pgwslider.css">
+</head>
+
+<SCRIPT type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+return false}
+</SCRIPT>
+
+<body>
+<div class="main">
+	<ul class="pgwSlider">
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/ағаш.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/дастархан.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жаңа жыл.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жаңа жылыңызбен.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жаңа жылыңызбен1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жапырақ жеміс жидектер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жапырақ.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жапырақ1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жеміс жидек гүл ыдыстар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жеміс жидек гүлдер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жеміс жидектер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жеміс көкөністер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жеміс көкөністер1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жеміс ыдыс.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жемістер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/жемістер1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/қызанақ.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/мереке.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/саңырауқұлақтар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/саңырауқұлақтар1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/себеттегі шие.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/талдар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/шырмауық гүлдер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/шырмауық гүлдер1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/шырша ағаштар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/ыдыстағы гүл жеміс жидектер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/jemis jidek osimdik agahtar/ыдыстағы жеміс жидектер.jpg"/></li>
+	</ul>
+
+<li class="тақырыптар">
+<a class="адамдар" href="https://qalpan.github.io/korme/адамдар">адамдар ....</a>
+<a class="бөлме-ғимарат-жобалар" href="https://qalpan.github.io/korme/бөлме-ғимарат-жобалар">бөлме ғимарат жобалар</a>
+<a class="ғарыш-қиал-тылсым-әлем" href="https://qalpan.github.io/korme/ғарыш-қиал-тылсым-әлем">ғарыш қиал тылсым әлем</a>
+<a class="гүлдер" href="https://qalpan.github.io/korme/гүлдер">гүлдер</a>
+<a class="жеміс-жидек-өсімдік-ағаштар" href="https://qalpan.github.io/korme/жеміс-жидек-өсімдік-ағаштар"><p class="takyryp-janu">жеміс жидек өсімдік ағаштар</p></a>
+<a class="қалыпталған-кескіндер" href="https://qalpan.github.io/korme/қалыпталған-кескіндер">қалыпталған кескіндер</a>
+<a class="қолданыс-заттары-құрылғы-құралдар" href="https://qalpan.github.io/korme/қолданыс-заттары-құрылғы-құралдар">қолданыс заттары құрылғы құралдар</a>
+<a class="құс-жануар-жәндік-балықтар" href="https://qalpan.github.io/korme/құс-жануар-жәндік-балықтар">құс жануар жәндік балықтар</a>
+<a class="өнер-білім-спорт" href="https://qalpan.github.io/korme/өнер-білім-спорт">өнер білім спорт</a>
+<a class="сән-үлгілері" href="https://qalpan.github.io/korme/сән-үлгілері">сән үлгілері</a>
+<a class="табиғат" href="https://qalpan.github.io/korme/табиғат">табиғат</a></li>
+
+<ul id="атаубелгі-karlyga">
+<a class="белгі-karlyga">
+<span class="karlyga">Қalpan</span>kөrмеsі</a>
+</ul>
+
+</div>
+
+<script>
+	$(document).ready(function() {
+		$('.pgwSlider').pgwSlider({
+			autoSlide: true,
+			displayControls: true
+		});
+	});
+</script>
+
+</body>
+</html>
