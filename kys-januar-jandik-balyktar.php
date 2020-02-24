ebe2bd4bb1f408bdf7dfeeb7475066a79431f4b8
kөrme <61244717+qalpan@users.noreply.github.com>
2020-02-24 18:13:45 +0600

    Add files via upload

---
+++ kys-januar-jandik-balyktar.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://qalpan.github.io/korme/suretter/favicon.gif" type="image/gif">
+	<title>Қaлпан</title>
+	<script src="https://qalpan.github.io/korme/js/jquery.min.js"></script>
+	<script src="https://qalpan.github.io/korme/js/pgwslider.js"></script>
+	<link rel="stylesheet" href="https://qalpan.github.io/korme/css/pgwslider.css">
+</head>
+
+<SCRIPT type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+return false}
+</SCRIPT>
+
+<body>
+<div class="main">
+	<ul class="pgwSlider">
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/ақ делпін.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/ақ делпін1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/аққулар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/ала балық.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/алтын бақа.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/алтын балық.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/аң құстар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/балапандар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/балықтар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/балықтар1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/бөкен.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/бөрі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/делпін.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/ерке қойан.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/жануарлар.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/кәдүмгі делпін.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/көгершін.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/көк кит.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/көк көбелек.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/көк көбелек1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/көк қызыл көбелек.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/көл асты.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қасат.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қашалот.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қонақ қоңыз ауылда.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қонақ қоңыз.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қоңыз жәндік.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қоңыз көбелектер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/құс ұйасы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/құстар ұйасы.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қызыл сары көбелек.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/қыстағы торғай.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/маймыл тышқан 1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/маймыл тышқан.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/медұз.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/мишөн.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/мысық ару.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/орман қойаны.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/орман қойаны1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/панды.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/сар балапан.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/сар балапан1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/сар балапан2.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/сары көбелек.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/сары түлкі.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/сәйгүлік.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/сөйлейтін балық.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/томпақ қойан.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/тропика кеші.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/түсті түлкілер.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/ханшайым балық.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/kys januar jandik balyktar/шырша ханзада балық.jpg"/></li>
+	</ul>
+
+<li class="тақырыптар">
+<a class="адамдар" href="https://qalpan.github.io/korme/index.html">адамдар ....</a>
+<a class="бөлме-ғимарат-жобалар" href="https://qalpan.github.io/korme/bolme-gimarat-jobalar.html">бөлме ғимарат жобалар</a>
+<a class="ғарыш-қиал-тылсым-әлем" href="https://qalpan.github.io/korme/garyh-kial-tylsym-alem.html">ғарыш қиал тылсым әлем</a>
+<a class="гүлдер" href="https://qalpan.github.io/korme/gylder.html">гүлдер</a>
+<a class="жеміс-жидек-өсімдік-ағаштар" href="https://qalpan.github.io/korme/jemis-jidek-osimdik-agahtar.html">жеміс жидек өсімдік ағаштар</a>
+<a class="қалыпталған-кескіндер" href="https://qalpan.github.io/korme/kalyptalgan-keskinder.html">қалыпталған кескіндер</a>
+<a class="қолданыс-заттары-құрылғы-құралдар" href="https://qalpan.github.io/korme/koldanys-zattary-kyrylgy-kyraldar.html 
+">қолданыс заттары құрылғы құралдар</a>
+<a class="құс-жануар-жәндік-балықтар" href="https://qalpan.github.io/korme/құс-жануар-жәндік-балықтар"><p class="takyryp-janu">құс жануар жәндік балықтар</p></a>
+<a class="өнер-білім-спорт" href="https://qalpan.github.io/korme/өнер-білім-спорт">өнер білім спорт</a>
+<a class="сән-үлгілері" href="https://qalpan.github.io/korme/сән-үлгілері">сән үлгілері</a>
+<a class="табиғат" href="https://qalpan.github.io/korme/табиғат">табиғат</a></li>
+
+<ul id="атаубелгі-karlyga">
+<a class="белгі-karlyga">
+<span class="karlyga">Қalpan</span>kөrмеsі</a>
+</ul>
+</div>
+
+<script>
+	$(document).ready(function() {
+		$('.pgwSlider').pgwSlider({
+			autoSlide: true,
+			displayControls: true
+		});
+	});
+</script>
+
+</body>
+</html>
