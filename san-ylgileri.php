a53ea7984f95e6151a2d6be1bd8d42b6095eaadc
kөrme <61244717+qalpan@users.noreply.github.com>
2020-02-24 18:32:40 +0600

    Add files via upload

---
+++ san-ylgileri.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://qalpan.github.io/korme/suretter/favicon.gif" type="image/gif">
+	<title>Қaлпан</title>
+	<script src="https://qalpan.github.io/korme/js/jquery.min.js"></script>
+	<script src="https://qalpan.github.io/korme/js/pgwslider.js"></script>
+	<link rel="stylesheet" href="https://qalpan.github.io/korme/css/pgwslider.css">
+</head>
+
+<SCRIPT type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+return false}
+</SCRIPT>
+
+<body>
+<div class="main">
+	<ul class="pgwSlider">
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/барби заттары.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/бөрік.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән1.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән2.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән3.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән4.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән5.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән6.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән7.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән8.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән9.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән10.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән11.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән12.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән13.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән14.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сән15.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/сәукеле.jpg"/></li>
+<li><img src="https://qalpan.github.io/korme/suretter/san ylgileri/шапан.jpg"/></li>
+	</ul>
+
+<li class="тақырыптар">
+<a class="адамдар" href="https://qalpan.github.io/korme/index.html">адамдар ....</a>
+<a class="бөлме-ғимарат-жобалар" href="https://qalpan.github.io/korme/bolme-gimarat-jobalar.html">бөлме ғимарат жобалар</a>
+<a class="ғарыш-қиал-тылсым-әлем" href="https://qalpan.github.io/korme/garyh-kial-tylsym-alem.html">ғарыш қиал тылсым әлем</a>
+<a class="гүлдер" href="https://qalpan.github.io/korme/gylder.html">гүлдер</a>
+<a class="жеміс-жидек-өсімдік-ағаштар" href="https://qalpan.github.io/korme/jemis-jidek-osimdik-agahtar.html">жеміс жидек өсімдік ағаштар</a>
+<a class="қалыпталған-кескіндер" href="https://qalpan.github.io/korme/kalyptalgan-keskinder.html">қалыпталған кескіндер</a>
+<a class="қолданыс-заттары-құрылғы-құралдар" href="https://qalpan.github.io/korme/koldanys-zattary-kyrylgy-kyraldar.html 
+">қолданыс заттары құрылғы құралдар</a>
+<a class="құс-жануар-жәндік-балықтар" href="https://qalpan.github.io/korme/kys-januar-jandik-balyktar.html">құс жануар жәндік балықтар</a>
+<a class="өнер-білім-спорт" href="https://qalpan.github.io/korme/өнер-білім-спорт">өнер білім спорт</a>
+<a class="сән-үлгілері" href="https://qalpan.github.io/korme/сән-үлгілері"><p class="takyryp-janu">сән үлгілері</p></a>
+<a class="табиғат" href="https://qalpan.github.io/korme/табиғат">табиғат</a></li>
+
+<ul id="атаубелгі-karlyga">
+<a class="белгі-karlyga">
+<span class="karlyga">Қalpan</span>kөrмеsі</a>
+</ul>
+</div>
+<script>
+	$(document).ready(function() {
+		$('.pgwSlider').pgwSlider({
+			autoSlide: true,
+			displayControls: true
+		});
+	});
+</script>
+</body>
+</html>
