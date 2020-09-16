4c1f0b2f7fc649d62358aabc05ea4e0148550451
kөrme <61244717+qalpan@users.noreply.github.com>
2020-09-16 13:49:39 +0600

    Update unitegallery.js

--- js/unitegallery.js
+++ js/unitegallery.js
@@ -9197,9 +9197,9 @@ function UGSlider(){
 		  slider_play_button_skin:"",					 //skin of the slider play button, if empty inherit from gallery skin
 		  slider_play_button_align_hor:"right",    		 //left, center, right - play button horizontal align
 		  slider_play_button_align_vert:"bottom",     	 //top, middle, bottom - play button vertical align
-		  slider_play_button_offset_hor:10,	       		 //play button horizontal offset
-		  slider_play_button_offset_vert:20,             //play button vertical offset
-		  slider_play_button_mobilehide:false,		 	 //hide the play button on mobile
+		  slider_play_button_offset_hor:20,	       		 //play button horizontal offset
+		  slider_play_button_offset_vert:40,             //play button vertical offset
+		  slider_play_button_mobilehide:true,		 	 //hide the play button on mobile
 
 		  slider_enable_fullscreen_button:false,		     //true,false - enable fullscreen button onslider element
 		  slider_fullscreen_button_skin:"",			     //skin of the slider fullscreen button, if empty inherit from gallery skin
